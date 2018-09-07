dc1691d2ea233bd1733d9217f5947fe86bf51d9e
shreelimbkar <limbkarshree@gmail.com>
2018-09-07 16:56:15 +0530

    added redux-devtools-extension
    added redux-devtools-extension

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState);
+    return createStore(
+        rootReducer, 
+        initialState,
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+        composeWithDevTools (
+            applyMiddleware()
+        )
+    );
 }
